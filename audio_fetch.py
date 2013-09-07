aa7b871d6cb8f9218a2626ede5753fc7776475dc
Scott Lawrence <bytbox@gmail.com>
2013-09-07 17:20:40 -0400

    Some fixes

--- audio_fetch.py
+++ audio_fetch.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
 import terpsichore
+import wave
+import numpy as np
+from StringIO import StringIO
 
 class UploadFile():
-	def __init__(self, file=None):
-		self.file = file
-		try:
-			self.wave = wave.open(self.file)
-		except:
-			raise Exception("could not read a wave file")
-		if self.file != None:
-			self.parse()
+	def __init__(self, f):
+		self.f = StringIO(f)
+                self.wave = wave.open(self.f)
+                self.parse()
 	def parse(self):
  		if self.wave.getnchannels() != 1:
  			print("Too many channels!")
@@ -22,5 +21,5 @@ class UploadFile():
 		self.data = np.fromstring(self.wave.readframes(self.wave.getnframes()), dtype=np.int16)
 		self.wave.close()
  
-		self.transcriber = Transcriber(self.framerate, handle_note)
+		self.transcriber = terpsichore.Transcriber(self.framerate, handle_note)
 		self.transcriber.process(self.data)
