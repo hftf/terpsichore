e42850c8da573efc4005ecd735d056d1fe841ff7
Nicolas Homble <nicolas.homble@gmail.com>
2013-09-07 04:59:28 -0400

    More comments and skeleton work, driver now has multiple pages for redirection if needed

--- gae/driver.py
+++ gae/driver.py
@@ -3,9 +3,16 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
 #	upon a get request
 	def get(self):
+		self.response.headers['Content-Type'] = 'text/html'
+		self.response.write('main page')
+
+class DevPage(webapp2.RequestHandler):
+	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('indev')
-	
+		self.response.write('dev page')
+
 application = webapp2.WSGIApplication([
+#	define the application tree here
 	('/', MainPage),
+	('/dev', DevPage),
 ], debug=True)
