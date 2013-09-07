6133bb90a26ff25fc534269c4ed2854e1210c205
Scott Lawrence <bytbox@gmail.com>
2013-09-07 11:45:15 -0400

    Some notes working kinda. Almost chords

--- terpsichore.py
+++ terpsichore.py
@@ -9,12 +9,24 @@ import wave
 
 import matplotlib.pyplot as plt
 
-def fourier(wave):
-    return zip([x for x in fft.fftfreq(len(wave)) if x >= 0], np.absolute(fft.rfft(wave).real))
+def fourier(wave, fr):
+    return zip([x * fr for x in fft.fftfreq(len(wave)) if x > 0], np.absolute(fft.rfft(wave).real[1:]))
 
-#x = [np.sin(x * np.pi / 4) for x in np.arange(1,1000)]
+NOTES = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
 
-#http://docs.python.org/3.1/library/wave.html
+def freq2note(f):
+    l = np.log(f / 440) / np.log(np.power(2, 1/12))
+    n = np.int(np.round(l))
+    note = NOTES[np.mod(n, 12)]
+    octave = np.int(n / 12) + 4
+    cents = np.round(100 * (l - n))
+    return (note, octave)
+
+class Transcriber:
+    def __init__(self, add_note):
+        self.add_note = add_note
+
+    #def 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -30,21 +42,37 @@ if __name__ == '__main__':
         print("Bad sample width!")
         sys.exit(1)
     fr = wav.getframerate()
-    data = np.fromstring(wav.readframes(10000), dtype=np.int16)
+    data = np.fromstring(wav.readframes(wav.getnframes()), dtype=np.int16)
     wav.close()
 
-    #print(wav.readframes(40000))
+    samplesize = 4000
+    samplestart = 0
+    current = {}
+    while samplestart + samplesize < len(data):
+        sample = data[samplestart:samplestart+samplesize]
+        f = sorted(fourier(sample, fr), key=operator.itemgetter(1), reverse=True)
+        cs = [(freq2note(x[0]), x[1]) for x in f[:10] if x[1] > 10]
 
-    f = sorted(fourier(data), key=operator.itemgetter(1), reverse=True)
-    print([x[0] * 40000 for x in f][:80])
-
+        notes = [c[0] for c in cs]
+        kill = []
+        for note in current:
+            if not note in notes:
+                dur = current[note] * samplesize/2 / fr
+                if dur > 0.8:
+                    print('{} {}'.format(note, dur))
+                kill.append(note)
+        for k in kill:
+            del(current[k])
+        for note in notes:
+            if note in current:
+                current[note] = current[note] + 1
+            else:
+                current[note] = 1
+        samplestart += samplesize/2
 """
-    SAMPLE_RATE = 44100
-    data = np.sin(5000 * 2 * np.pi * np.arange(40000) / 40001)
+        f = list(fourier(sample, samplesize))
+        plt.plot([x[0] for x in f], [x[1] for x in f])
+        plt.xlim([0, 1200])
+        #plt.show()
+        """
 
-    f = list(fourier(data))
-    plt.plot([x[0] for x in f], [x[1] for x in f])
-    plt.show()
-    f = sorted(fourier(data), key=operator.itemgetter(1), reverse=True)
-    print([x[0] * 40000 for x in f][:80])
-    """
