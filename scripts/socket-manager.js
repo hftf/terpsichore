1af18ae1dc1c25c26267894c875321e69ba008be
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-09-08 07:52:15 -0400

    Revert "removing"
    This reverts commit 1c0f1564731159e0702e7b5478b3aa69fa626df3.

--- scripts/socket-manager.js
+++ scripts/socket-manager.js
@@ -1,16 +1,35 @@
-var postArray = function(array) {
-    string = "";
-    for (var i = 0; i < array.length; i ++) {
+postArray = function(array) {
+    // path = '/record';
+    // var xhr = new XMLHttpRequest();
+    // xhr.open('POST', path, true);
+    // xhr.send(array);
+    string = ""
+    for(var  i = 0; i < array.length; i++) {
         array[i] *= 32768
         string += Math.round(array[i]) + ";";
     }
-    
     $.ajax({
-        'type': 'POST',
-        'url': '/record',
-        'data': { 'data': string, 'token': myToken, 'flag': 'ok' },
-        success: function(data) {
-            console.log(data);
+        'type' : 'POST',
+        'url' : '/record',
+        'data' : {'data': string, 'token' : myToken, 'flag' : 'ok'},
+        success : function(data){
+            console.log(data)
         }
     });
 };
+
+onOpened = function() {
+    connected = true;
+};
+
+onMessage = function(){
+
+}
+
+onError = function(){
+
+}
+
+onClose = function(){
+
+}
